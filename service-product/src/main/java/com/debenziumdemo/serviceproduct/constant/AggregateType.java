db18b6eac95d2a0057cf1d0d88de98f40fbad079
larryjhong <larryjhong@fontrip.com>
2025-05-08 06:03:00 +0800

    [task03]build service-order kafka event

--- service-product/src/main/java/com/debenziumdemo/serviceproduct/constant/AggregateType.java
+++
@@ -1,7 +0,0 @@
-package com.debenziumdemo.serviceproduct.constant;
-
-public enum AggregateType {
-    ORDER,
-    PRODUCT,
-    POINT
-}
--- service-product/src/main/java/com/debenziumdemo/serviceproduct/constant/EventType.java
+++
@@ -1,9 +0,0 @@
-package com.debenziumdemo.serviceproduct.constant;
-
-public enum EventType {
-    ORDER_CREATED,
-    PRODUCT_CREATED,
-    PRODUCT_UPDATED,
-    POINT_EARNED,
-    POINT_REDEEMED
-}
